61890446f18ed84534a933a038bc793e17ce1b97
Sokrat Poghosyan <sorkat.poghosyan@gmail.com>
2020-09-29 23:04:12 +0400

    Remove insta icon from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import InstaFooterIcon from 'icons/InstaFooterIcon';
 
 const App = () => {
   return (
     <div>
       React | Typescript | Sass boilerplate
-      <InstaFooterIcon />
     </div>
   );
 }
