9fbf451d4bac07737017f1eb9b94900014a81092
Varsine <varsulmozg@gmail.com>
2020-09-30 14:50:04 +0400

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react"
+
+import Header from "./containers/Header/Header"
+import Footer from './containers/Footer/Footer'
+import Routes from './Routes'
 
 const App = () => {
-  return (
-    <div>
-      React | Typescript | Sass boilerplate
-    </div>
-  );
+  return <div>
+    <Header/>
+    <Routes/>
+    <Footer/>
+  </div>
 }
 
-export default App;
+export default App
---
+++ src/Routes.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Router, Link } from "@reach/router"
+
+import Landing from './containers/Landing/Landing'
+import Quiz from './containers/Quiz/Quiz'
+import OrderDetails from './containers/OrderDetails/OrderDetails'
+import Checkout from './containers/Checkout/Checkout'
+import Login_Signup from './containers/Login_Signup/Login_Signup'
+
+
+interface IRoutesProps {};
+
+const Routes: React.FC<IRoutesProps> = () => {
+    return <Router>
+        <Landing path="/"/>
+        <Quiz path="/quiz"/>
+        <OrderDetails path="/order-details"/>
+        <Checkout path="/checkout"/>
+        <Login_Signup path="/login"/>
+    </Router>
+}
+
+
+export default Routes;
