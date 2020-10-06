b8a42b85557270ad3b7c9bed3475a3210f3a4c9f
Sokrat Poghosyan <sorkat.poghosyan@gmail.com>
2020-10-06 23:26:41 +0400

    input field final

--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+import InputField from "components/InputField/InputField";
+import ProfileHeaderIcon from "icons/ProfileHeaderIcon";
 
 import "./Landing.scss"
 
 interface ILandingProps { }
 
 const Landing: React.FC<ILandingProps> = () => {
+  const [value, setValue] = useState('');
+  const [hasError, setHasError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onChangeHandler = (val: string) => {
+    if(val.length < 4) {
+      setHasError(true);
+    } else {
+      setHasError(false);
+    }
+    setValue(val);
+  }
 
   return (
-    <div>
-      
+    <div className="app-landing">
+      <InputField
+        onChange={onChangeHandler}
+        value={value}
+        Icon={ProfileHeaderIcon}
+        name={'Profile Name input'}
+        placeholder={'Enter your Name...'}
+        type={'password'}
+        // hasError={hasError}
+        loading={false}
+      />
     </div>
   )
 }
