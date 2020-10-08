451fa34d6937a15271fa896558c4f4dcdb23427f
Varsine <varsulmozg@gmail.com>
2020-10-08 14:49:02 +0400

    header-media

--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -7,7 +7,7 @@ interface ILandingProps {}
 const Landing: React.FC<ILandingProps> = () => {
   return (
     <div>
-      sasdhbsah bdhsbah bddf hsbfh bdhsnbf dhnbsfjnnd jfnj dksf
+      sasdhbsah bdhsbah bddf hsbfh bdhsnbf dhnbsfjnnd jfnj dksf.
     </div>
   )
 }
