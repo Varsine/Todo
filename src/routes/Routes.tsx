c017e3d026f4f33ed65a5adc60941af2e1de9097
Sokrat Poghosyan <sorkat.poghosyan@gmail.com>
2020-11-05 17:47:09 +0400

    Fix routes issue and other bugs

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -7,18 +7,24 @@ const Quiz = loadable(({ path }) => import("containers/Quiz/Quiz"));
 const OrderDetails = loadable(({ path }) => import("containers/OrderDetails/OrderDetails"))
 const Checkout = loadable(({ path }) => import("containers/Checkout/Checkout"));
 const Auth = loadable(({ path }) => import("containers/Auth/Auth"));
+const Header = loadable(({ path }) => import("containers/Header/Header"));
 
 interface IRoutesProps { }
 
 const Routes: React.FC<IRoutesProps> = () => {
   return (
-    <Router>
-      <Landing path="/" />
-      <Quiz path="/quiz" />
-      <OrderDetails path="/order-details" />
-      <Checkout path="/checkout" />
-      <Auth path="/auth" />
-    </Router>
+    <>
+      <Router primary={false}>
+        <Header path="*" />
+      </Router>
+      <Router>
+        <Landing path="/" />
+        <Quiz path="/quiz" />
+        <OrderDetails path="/order-details" />
+        <Checkout path="/checkout" />
+        <Auth path="/auth" />
+      </Router>
+    </>
   )
 }
 
