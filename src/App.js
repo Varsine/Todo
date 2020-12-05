1c30a4652b0013124409a6b0e4605bc4c21b97b5
Varsine <varsulmozg@gmail.com>
2020-12-05 21:30:23 +0400

    sss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import firebase from "firebase/app"
+import "firebase/auth"
 
+
+import {firebaseConfig} from "api/firebaseConfig"
 import {appReducer} from 'reducer/appReducer';
 import Todo from './containers/Todo/Todo';
 
 import "./global.scss"
+
+firebase.initializeApp(firebaseConfig)
 
 const store = createStore(appReducer)
 
