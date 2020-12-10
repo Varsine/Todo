7b45086c3db226bc69d2b2c29eca143a377b9c83
Varsine <varsulmozg@gmail.com>
2020-12-10 11:27:26 +0400

    Updates

---
+++ precache-manifest.d38acd8808db0bf73a73bde3b2fdf0c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d68d8fbf9694f7b1f0db4cb2f4d998e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "ec91319e34304b222e4d",
+    "url": "/todo-list/static/css/main.0f2ba73e.chunk.css"
+  },
+  {
+    "revision": "b16e0d81b5fd3baccc79",
+    "url": "/todo-list/static/js/2.68358ca2.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/todo-list/static/js/2.68358ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec91319e34304b222e4d",
+    "url": "/todo-list/static/js/main.28ae754d.chunk.js"
+  },
+  {
+    "revision": "27e019a4eb32999bd0bf",
+    "url": "/todo-list/static/js/runtime-main.88bd720a.js"
+  }
+]);
